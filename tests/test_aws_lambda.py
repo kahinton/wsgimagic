8b408984ca6ac854dfc9c2f9bf138db3ef2474dd
khinton <khinton@aiminspections.com>
2018-09-18 15:17:35 -0400

    changed unit tests for the aws_lambda package to account for refactor

--- tests/test_aws_lambda.py
+++ tests/test_aws_lambda.py
@@ -4,19 +4,19 @@ import os
 import sys
 
 
-
 lib_dir = os.path.join(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')), 'wsgimagic')
 if lib_dir not in sys.path:
     sys.path.insert(1, lib_dir)
 
-from aws_lambda import _WSGIHandler, _basic_error_handler, _APIGatewayEvent
+from aws_lambda import _map_api_gateway_to_request, _basic_error_handler, _build_proxy_response, \
+    TranslatedRequest
 
 
 fake_event = {
     "resource": None,
     "path": '/test',
     "httpMethod": 'GET',
-    "headers": {},
+    "headers": {"test": "test"},
     "queryStringParameters": {'t': 'test', 't2': 'test2'},
     "pathParameters":  {},
     "stageVariables": {},
@@ -25,40 +25,56 @@ fake_event = {
     "isBase64Encoded": False
 }
 
-fake_API_event = _APIGatewayEvent(**fake_event)
 
+class FakeWSGIHandler:
+    caught_exception = None
+    response_status = '200 Ok'
+    outbound_headers = {'Header1': 'H1',
+                        'Header2': 'H2'}
 
-class FakeWSGIApp:
-    pass
+    def error_handler(self, e):
+        return "Something Bad Happened"
 
 
 class TestWsgiMagic(unittest.TestCase):
-    def test_get_environment_dictionary(self):       
-        test_env = _WSGIHandler.generate_env_dict(fake_API_event, 'localhost', 80)
-        del(test_env['wsgi.input'])
-        del(test_env['wsgi.errors'])
-        self.assertEqual(test_env, {'wsgi.version': (1, 0), 'wsgi.url_scheme': 'http', 
-                                    'wsgi.multithread': False, 'wsgi.multiprocess': False, 
-                                    'wsgi.run_once': False, 'REQUEST_METHOD': 'GET', 'PATH_INFO': '/test', 
-                                    'SERVER_NAME': 'localhost', 'SERVER_PORT': '80', 'QUERY_STRING': 't=test&t2=test2'})
+    def test__map_api_gateway_to_request(self):
+        # SETUP
+        translated = _map_api_gateway_to_request(**fake_event)
 
-    def test_wsgi_callback(self):
-        test_handler = _WSGIHandler(MagicMock(), None, 'localhost', 80, _basic_error_handler)
-        test_handler.wsgi_callback(status='200 OK', response_headers=[('H1', 'Header1'), ('H2', 'Header2')], exc_info=None)
-        self.assertEqual(test_handler.response_status, '200 OK')
-        self.assertEqual(test_handler.outbound_headers['H1'], 'Header1')
-        self.assertEqual(test_handler.outbound_headers['H2'], 'Header2')
-        self.assertEqual(test_handler.outbound_headers['Server'], 'WSGIMagic')
+        # ASSERT
+        needed_result = TranslatedRequest('/test', 'GET', {'HTTP_TEST': 'test'},
+                                          't=test&t2=test2', 'test')
 
-    def test_build_proxy_response(self):
-        test_handler = _WSGIHandler(MagicMock(), None, 'localhost', 80, _basic_error_handler)
-        test_handler.wsgi_callback(status='200 OK', response_headers=[('H1', 'Header1'), ('H2', 'Header2')], exc_info=None)
-        test_result = test_handler.build_proxy_response(['Hello ', 'World'])
-        self.assertEqual(test_result['statusCode'], '200')
-        self.assertEqual(test_result['headers']['H1'], 'Header1')
-        self.assertEqual(test_result['headers']['H2'], 'Header2')
-        self.assertEqual(test_result['headers']['Server'], 'WSGIMagic')
-        self.assertEqual(test_result['body'], 'Hello World')
+        self.assertEqual(translated.__dict__, needed_result.__dict__)
+
+    def test__basic_error_handler(self):
+        # SETUP
+        bad_result = _basic_error_handler(ValueError('An Error Occurred'))
+        del(bad_result['headers']['Date'])
+
+        # ASSERT
+        self.assertEqual(bad_result, {'statusCode': '500',
+                                      'headers': {'Server': 'WSGIMagic'},
+                                      'body': 'Server Error'})
+
+    def test__build_proxy_response_when_all_goes_well(self):
+        # SETUP
+        test_handler = FakeWSGIHandler()
+
+        # ASSERT
+        self.assertEqual(_build_proxy_response(test_handler, ['Hello', 'World']),
+                         {'statusCode': '200',
+                          'headers': {'Header1': 'H1', 'Header2': 'H2'},
+                          'body': 'HelloWorld'})
+
+    def test__build_proxy_response_when_an_error_occurred(self):
+        # SETUP
+        test_handler = FakeWSGIHandler()
+        test_handler.caught_exception = ValueError('Something Broke!')
+
+        # ASSERT
+        self.assertEqual(_build_proxy_response(test_handler, ['Hello', 'World']),
+                         "Something Bad Happened")
 
 
 if __name__ == '__main__':
