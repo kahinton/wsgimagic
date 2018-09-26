c78a013d7a9e22a48089bb7019e6a48b543e407d
khinton <khinton@aiminspections.com>
2018-09-26 10:38:11 -0400

    unit tests updated to support new functionality

--- tests/test_magic_core.py
+++ tests/test_magic_core.py
@@ -8,7 +8,7 @@ lib_dir = os.path.join(os.path.abspath(os.path.join(os.path.dirname(__file__), '
 if lib_dir not in sys.path:
     sys.path.insert(1, lib_dir)
 
-from magic_core import WSGIHandler, TranslatedRequest
+from magic_core import WSGIHandler, TranslatedRequest, RawResponse
 
 
 fake_API_event = TranslatedRequest('/test', 'GET', {'HTTP_TEST': 'test'}, 't=test&t2=test2',
@@ -17,21 +17,6 @@ fake_API_event = TranslatedRequest('/test', 'GET', {'HTTP_TEST': 'test'}, 't=tes
 
 class FakeWSGIApp:
     pass
-
-
-def fake_build_response(self, result: 'iterable') -> dict:
-    """Once the application completes the request, maps the results into the format required by
-    AWS.
-    """
-    if self.caught_exception is not None:
-        return self.error_handler(self.caught_exception)
-    return {'status': self.response_status.split(' ')[0],
-            'headers': self.outbound_headers,
-            'body': ''.join([str(message) for message in result])}
-
-
-def fake_error_handler(exception: Exception):
-    return 'You broke it!'
 
 
 class TestWsgiMagic(unittest.TestCase):
@@ -47,38 +32,14 @@ class TestWsgiMagic(unittest.TestCase):
                                     'QUERY_STRING': 't=test&t2=test2'})
 
     def test_wsgi_callback(self):
-        test_handler = WSGIHandler(MagicMock(), None, 'localhost', 80, fake_build_response,
-                                   fake_error_handler)
+        test_handler = WSGIHandler(MagicMock(), None, 'localhost', 80)
         test_handler.wsgi_callback(status='200 OK', response_headers=[('H1', 'Header1'),
                                                                       ('H2', 'Header2')],
                                    exc_info=None)
-        self.assertEqual(test_handler.response_status, '200 OK')
-        self.assertEqual(test_handler.outbound_headers['H1'], 'Header1')
-        self.assertEqual(test_handler.outbound_headers['H2'], 'Header2')
-        self.assertEqual(test_handler.outbound_headers['Server'], 'WSGIMagic')
-
-    def test_build_proxy_response(self):
-        test_handler = WSGIHandler(MagicMock(), None, 'localhost', 80, fake_build_response,
-                                   fake_error_handler)
-        test_handler.wsgi_callback(status='200 OK', response_headers=[('H1', 'Header1'),
-                                                                      ('H2', 'Header2')],
-                                   exc_info=None)
-        test_result = test_handler.build_response(test_handler, ['Hello ', 'World'])
-        self.assertEqual(test_result['status'], '200')
-        self.assertEqual(test_result['headers']['H1'], 'Header1')
-        self.assertEqual(test_result['headers']['H2'], 'Header2')
-        self.assertEqual(test_result['headers']['Server'], 'WSGIMagic')
-        self.assertEqual(test_result['body'], 'Hello World')
-
-    def test_ensure_error_handler_called_when_request_not_processed_correctly(self):
-        test_handler = WSGIHandler(MagicMock(), 'This should be a dict', 'localhost', 80,
-                                   fake_build_response, fake_error_handler)
-        test_handler.wsgi_callback(status='200 OK', response_headers=[('H1', 'Header1'),
-                                                                      ('H2', 'Header2')],
-                                   exc_info=None)
-
-        test_result = test_handler.build_response(test_handler, [])
-        self.assertEqual(test_result, 'You broke it!')
+        self.assertEqual(test_handler.response.response_status, '200 OK')
+        self.assertEqual(test_handler.response.outbound_headers['H1'], 'Header1')
+        self.assertEqual(test_handler.response.outbound_headers['H2'], 'Header2')
+        self.assertEqual(test_handler.response.outbound_headers['Server'], 'WSGIMagic')
 
 
 if __name__ == '__main__':
